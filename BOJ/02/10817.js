2329ed06c32ef325c7f355d0bf0d13b9cc8d4d78
Sungchan Yi <calofmijuck@snu.ac.kr>
2019-11-26 18:15:22 +0900

    Format files

--- BOJ/02/10817.js
+++ BOJ/02/10817.js
@@ -3,10 +3,10 @@ var s = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ');
 var s = s.map(e => parseInt(e));
 var a = s[0], b = s[1], c = s[2];
 
-if(a >= b) {
-    if(b >= c) console.log(b);
+if (a >= b) {
+    if (b >= c) console.log(b);
     else console.log(Math.min(a, c));
 } else {
-    if(a >= c) console.log(a);
+    if (a >= c) console.log(a);
     else console.log(Math.min(b, c));
 }
--- BOJ/03/2742.js
+++ BOJ/03/2742.js
@@ -1,7 +1,7 @@
 var s = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 s = parseInt(s);
 var ans = "";
-for(var i = s; i > 0; --i) {
+for (var i = s; i > 0; --i) {
     ans += i + '\n';
 }
 console.log(ans);
